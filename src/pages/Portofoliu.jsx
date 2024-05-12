a552502a455cd12a7f1359ac03d417f0ac7c9eae
Ajh108 <vladsimionescu98@gmail.com>
2024-05-12 12:30:28 +0300

    Deleting non used import

--- src/pages/Portofoliu.jsx
+++ src/pages/Portofoliu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { images, projectsData } from "../data/ProjectData";
+import { projectsData } from "../data/ProjectData";
 import Carrousel from "../components/Carousel";
 import "./Portofoliu.css";
 
